8ba9df1f997407f85e704a5d6acaadde93b8ad24
Jenis Lathiya <jenislathiya07@gmail.com>
2025-11-03 18:23:25 +0530

    Project Added

---
+++ src/views/dashboard/Dashboard2.tsx
@@ -0,0 +1,111 @@
+import ColorBox from 'src/components/dashboards/dashboard2/ColorBox';
+import DeveloperCard from 'src/components/dashboards/dashboard2/DeveloperCard';
+import FigmaCard from 'src/components/dashboards/dashboard2/FigmaCard';
+import NewGoal from 'src/components/dashboards/dashboard2/NewGoal';
+import ProductSales from 'src/components/dashboards/dashboard2/ProductSales';
+import ProfitExpense from 'src/components/dashboards/dashboard2/ProfitExpense';
+import TopEmployee from 'src/components/dashboards/dashboard2/TopEmployee';
+import TrafficDistribution from 'src/components/dashboards/dashboard2/TrafficDistribution';
+import UpcommingSchedule from 'src/components/dashboards/dashboard2/UpcommingSchedule';
+import VisitNow from 'src/components/dashboards/dashboard2/VisitNow';
+import Toaster from '../toaster';
+import { Button, Label, TextInput, Textarea } from 'flowbite-react';
+
+const Dashboard2 = () => {
+  return (
+    <>
+      <Toaster />
+      <div className="grid">
+        {/* <div className="lg:col-span-6 col-span-12">
+          <VisitNow />
+        </div>
+        <div className="lg:col-span-6 col-span-12">
+          <ColorBox />
+        </div>
+        <div className="lg:col-span-8 col-span-12">
+          <ProfitExpense />
+        </div>
+        <div className="lg:col-span-4 col-span-12">
+          <ProductSales />
+        </div>
+        <div className="lg:col-span-3 col-span-12">
+          <NewGoal />
+          <div className="mt-6">
+            <DeveloperCard />
+          </div>
+        </div>
+        <div className="lg:col-span-6 col-span-12">
+          <TrafficDistribution />{' '}
+        </div>
+        <div className="lg:col-span-3 col-span-12">
+          <FigmaCard />
+        </div>
+        <div className="lg:col-span-4 col-span-12">
+          <UpcommingSchedule />
+        </div>
+        <div className="lg:col-span-8 col-span-12">
+          <TopEmployee />
+        </div> */}
+
+        <div className='text-center bg-white border-gray-200 shadow-md p-6 rounded-[16px]'>
+          <h2 className='text-[24px] mb-3'>Feedback Form</h2>
+          <p>This Form is intended for a feature request on Drug Comparison. Please use the Contact Us button for all other day to day queries e.g. Products, Mapping Errors etc.</p>
+          <div className='text-left border-gray-200 rounded-[16px] p-6'>
+            <form className="mt-6">
+
+              <div className="mb-6">
+                <div className="mb-2 block">
+                  <Label htmlFor="name">Full Name<span className="text-red-500">*</span></Label>
+                </div>
+                <TextInput id="name" type="text" sizing="md" className="form-control" />
+              </div>
+
+              <div className="mb-6">
+                <div className="mb-2 block">
+                  <Label htmlFor="pharmacy_name">Pharmacy Name<span className="text-red-500">*</span></Label>
+                </div>
+                <TextInput id="pharmacy_name" type="text" sizing="md" className="form-control" />
+              </div>
+      
+              <div className="mb-6">
+                <div className="mb-2 block">
+                  <Label htmlFor="emadd">Email Address<span className="text-red-500">*</span></Label>
+                </div>
+                <TextInput id="emadd" type="text" sizing="md" className="form-control" />
+              </div>
+
+              <div className="mb-6">
+                <div className="mb-2 block">
+                  <Label htmlFor="postcode">Postcode:<span className="text-red-500">*</span></Label>
+                </div>
+                <TextInput id="postcode" type="text" sizing="md" className="form-control" />
+              </div>
+      
+              <div className="mb-6">
+                <div className="mb-2 block">
+                  <Label htmlFor="comment">Comments & Suggestions<span className="text-red-500">*</span></Label>
+                </div>
+                <Textarea
+                  rows={5}
+                  id="comment"
+                  className="w-full form-control-textarea"
+                />
+              </div>
+      
+              <div className='flex gap-4'>
+                <Button color="primary" size="lg" className="col-6 w-full">
+                  Submit
+                </Button>
+                <Button color="primary" size="lg" className="col-6 w-full relative flex items-center justify-center text-center font-medium focus:outline-none group p-0.5 focus:ring-0 cursor-pointer rounded-full px-5 text-sm bg-lighterror dark:bg-darkerror text-error hover:bg-error hover:text-white">
+                  Cancel
+                </Button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Dashboard2;
